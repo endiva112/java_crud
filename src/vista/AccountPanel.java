72049b7b7bd7eb6f0709c0ac26c7c83bc3ede9ad
Enrique Díaz Valenzuela <diazvalenzuelaenrique@gmail.com>
2025-12-15 22:10:16 +0100

    accountPanel 2.5/6

--- src/vista/AccountPanel.java
+++ src/vista/AccountPanel.java
@@ -6,6 +6,7 @@ import vista.componentes.Cabecera;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
 public class AccountPanel extends JPanel {
     private ControladorPrincipal controlador;
@@ -19,62 +20,108 @@ public class AccountPanel extends JPanel {
     }
 
     private void inicializarComponentes() {
+        // Colores
+        Color mainBG = new Color(34, 34, 59);
+        Color contrastBG = new Color(74, 78, 105);
+        Color panelBG = new Color(242, 233, 228);
+        Color buttonBG = new Color(170, 160, 165);
+        Color secondaryText = new Color(0, 0, 0);
+
         setLayout(new BorderLayout());
 
-        // Usar la NavBar reutilizable CON logout
+        // Cabecera
         add(new Cabecera(controlador, true), BorderLayout.NORTH);
 
-        // Panel central (datos)
+        // Panel central
         JPanel centerPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5);
+        centerPanel.setBackground(contrastBG);
 
-        // Avatar por defecto
-        gbc.gridx = 0; gbc.gridy = 0; gbc.gridwidth = 2;
+        JPanel mainCenterPanel = new JPanel();
+        mainCenterPanel.setBackground(panelBG);
+        mainCenterPanel.setLayout(new BoxLayout(mainCenterPanel, BoxLayout.Y_AXIS));
+        mainCenterPanel.setPreferredSize(new Dimension(500, 500));
+
+        JLabel title = new JLabel("Mi Cuenta");
+        title.setFont(new Font("Roboto", Font.BOLD, 32));
+        title.setForeground(secondaryText);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Avatar
         avatarLabel = new JLabel();
-        avatarLabel.setPreferredSize(new Dimension(100, 100));
-        avatarLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        ImageIcon icon = new ImageIcon("recursos/imagenes/userIcon.png");
-        Image img = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        avatarLabel.setPreferredSize(new Dimension(120, 120));
+        avatarLabel.setMaximumSize(new Dimension(120, 120));
+        avatarLabel.setBorder(BorderFactory.createLineBorder(new Color(100, 100, 100), 2));
+        avatarLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        URL urlAvatar = AccountPanel.class.getResource("/imagenes/icons/userIcon.png");
+        ImageIcon icon = new ImageIcon(urlAvatar);
+        Image img = icon.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
         avatarLabel.setIcon(new ImageIcon(img));
-        centerPanel.add(avatarLabel, gbc);
 
         // TODO: Implementar funcionalidad para cambiar avatar (JFileChooser)
 
-        // Email
-        gbc.gridwidth = 1;
-        gbc.gridx = 0; gbc.gridy = 2;
-        centerPanel.add(new JLabel("Email:"), gbc);
+        JLabel emailLabel = new JLabel("Correo electrónico");
+        emailLabel.setFont(new Font("Roboto", Font.PLAIN, 16));
+        emailLabel.setForeground(secondaryText);
+        emailLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        emailField = new JTextField(20);
-        gbc.gridx = 1;
-        centerPanel.add(emailField, gbc);
+        emailField = new JTextField();
+        emailField.setMaximumSize(new Dimension(360, 40));
 
-        // Password
-        gbc.gridx = 0; gbc.gridy = 3;
-        centerPanel.add(new JLabel("Nueva Password:"), gbc);
+        JLabel passwordLabel = new JLabel("Nueva contraseña");
+        passwordLabel.setFont(new Font("Roboto", Font.PLAIN, 16));
+        passwordLabel.setForeground(secondaryText);
+        passwordLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        passwordField = new JPasswordField(20);
-        gbc.gridx = 1;
-        centerPanel.add(passwordField, gbc);
+        passwordField = new JPasswordField();
+        passwordField.setMaximumSize(new Dimension(360, 40));
 
-        // Botón actualizar
         JButton btnActualizar = new JButton("Actualizar Datos");
+        btnActualizar.setFont(new Font("Roboto", Font.BOLD, 18));
+        btnActualizar.setForeground(secondaryText);
+        btnActualizar.setBackground(buttonBG);
+        btnActualizar.setFocusPainted(false);
+        btnActualizar.setBorderPainted(false);
+        btnActualizar.setOpaque(true);
+        btnActualizar.setPreferredSize(new Dimension(360, 50));
+        btnActualizar.setMaximumSize(new Dimension(360, 50));
+        btnActualizar.setAlignmentX(Component.CENTER_ALIGNMENT);
         btnActualizar.addActionListener(e -> {
             String email = emailField.getText();
             String password = new String(passwordField.getPassword());
             controlador.actualizarCuenta(email, password, null);
         });
-        gbc.gridx = 0; gbc.gridy = 4; gbc.gridwidth = 2;
-        centerPanel.add(btnActualizar, gbc);
 
-        // Botón eliminar cuenta
         JButton btnEliminar = new JButton("Eliminar Cuenta");
-        btnEliminar.setForeground(Color.RED);
+        btnEliminar.setFont(new Font("Roboto", Font.BOLD, 18));
+        btnEliminar.setForeground(Color.WHITE);
+        btnEliminar.setBackground(new Color(200, 50, 50));
+        btnEliminar.setFocusPainted(false);
+        btnEliminar.setBorderPainted(false);
+        btnEliminar.setOpaque(true);
+        btnEliminar.setPreferredSize(new Dimension(360, 50));
+        btnEliminar.setMaximumSize(new Dimension(360, 50));
+        btnEliminar.setAlignmentX(Component.CENTER_ALIGNMENT);
         btnEliminar.addActionListener(e -> controlador.eliminarCuenta());
-        gbc.gridy = 5;
-        centerPanel.add(btnEliminar, gbc);
 
+        mainCenterPanel.add(Box.createVerticalStrut(30));
+        mainCenterPanel.add(title);
+        mainCenterPanel.add(Box.createVerticalStrut(20));
+        mainCenterPanel.add(avatarLabel);
+        mainCenterPanel.add(Box.createVerticalStrut(30));
+        mainCenterPanel.add(emailLabel);
+        mainCenterPanel.add(Box.createVerticalStrut(8));
+        mainCenterPanel.add(emailField);
+        mainCenterPanel.add(Box.createVerticalStrut(20));
+        mainCenterPanel.add(passwordLabel);
+        mainCenterPanel.add(Box.createVerticalStrut(8));
+        mainCenterPanel.add(passwordField);
+        mainCenterPanel.add(Box.createVerticalStrut(30));
+        mainCenterPanel.add(btnActualizar);
+        mainCenterPanel.add(Box.createVerticalStrut(15));
+        mainCenterPanel.add(btnEliminar);
+
+        centerPanel.add(mainCenterPanel);
         add(centerPanel, BorderLayout.CENTER);
     }
 
